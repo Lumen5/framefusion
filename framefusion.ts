bce41303d713cb30faf5516eabf7aad609334a5f
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2023-04-26 11:14:02 -0400

    Actually keep that for now

--- framefusion.ts
+++ framefusion.ts
@@ -32,6 +32,8 @@ export interface Extractor {
 
     get height(): number;
 
+    getFrameAtTime(targetTime: number): Promise<Frame>;
+
     getFrameImageDataAtTime(targetTime: number): Promise<ImageData>;
 
     dispose(): Promise<void>;
