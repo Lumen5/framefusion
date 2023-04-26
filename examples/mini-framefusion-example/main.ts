6b512edc694711fe200684fab53858e97755ba14
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2023-04-26 11:26:12 -0400

    Drop the "Frame" + fix types

--- examples/mini-framefusion-example/main.ts
+++ examples/mini-framefusion-example/main.ts
@@ -9,7 +9,7 @@ async function run() {
     });
 
     // Get frame at a specific time (in seconds)
-    const frame = await extractor.getFrameImageDataAtTime(2.0);
+    const frame = await extractor.getImageDataAtTime(2.0);
 
     // Do something with frame data
     console.log(frame.data);
