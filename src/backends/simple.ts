13f79e16f5083353031ee460dadbc207477ef87a
animanathome <animanatwork@me.com>
2023-05-03 15:21:11 -0700

    adding support for seeking backwards

--- src/backends/simple.ts
+++ src/backends/simple.ts
@@ -69,6 +69,8 @@ const getFilter = async({
     });
 };
 
+const MAX_PACKET_READS = 1000;
+
 /**
  * A simple extractor that uses beamcoder to extract frames from a video file.
  */
@@ -108,6 +110,12 @@ export class SimpleExtractor extends BaseExtractor implements Extractor {
      * this ensure we don't skip any frames.
      */
     packet: Packet = null;
+
+    /**
+     * The last target presentation timestamp (PTS) we requested. If we never requested a time(stamp) then this
+     * value is null
+     */
+    previousTargetPTS: null | number = null;
 
     static async create(args: ExtractorArgs): Promise<Extractor> {
         const extractor = new SimpleExtractor();
@@ -174,7 +182,7 @@ export class SimpleExtractor extends BaseExtractor implements Extractor {
     async getImageDataAtTime(targetTime: number): Promise<ImageData> {
         const targetPts = Math.floor(this._timeToPTS(targetTime));
         console.log('targetTime', targetTime, '-> targetPts', targetPts);
-        const frame = await this._getFrameAtPts(targetTime, targetPts);
+        const frame = await this._getFrameAtPts(targetPts);
         if (!frame) {
             console.log('no frame found');
             return null;
@@ -193,30 +201,59 @@ export class SimpleExtractor extends BaseExtractor implements Extractor {
         return time * time_base[1] / time_base[0];
     }
 
-    async _getFrameAtPts(targetTime, targetPTS: number) {
-        console.log('_getFrameAtPts', targetTime, targetPTS, '-> duration', this.duration);
+    async _getFrameAtPts(targetPTS: number) {
+        console.log('_getFrameAtPts', targetPTS, '-> duration', this.duration);
         let packetReadCount = 0;
 
-        // console.log('seek', 'timestamp', targetTime, 'targetPTS', targetPTS);
-        // get the iframe before the targetPTS
-        // await this.demuxer.seek({
-        //     // stream_index: 0, // even though we specify the stream index, it still seeks all streams
-        //     time: targetTime,
-        //     // backward: true,
-        //     // any: false,
-        // });
-        // console.log('done seeking to', targetTime);
+        if (!this.previousTargetPTS) {
+            // this is the first time we're calling method, get the iframe closest to the targetPTS to reduce the number
+            // of packet reads
+            console.log(`seeking forwards to ${targetPTS}`);
+            await this.demuxer.seek({
+                stream_index: 0, // even though we specify the stream index, it still seeks all streams
+                timestamp: targetPTS,
+                any: false,
+            });
+            this.packet = null;
+            this.frames = [];
+        }
+        else if (this.previousTargetPTS > targetPTS) {
+            // we're looking backwards
+            console.log(`seeking backwards to ${targetPTS}`);
+            await this.demuxer.seek({
+                stream_index: 0, // even though we specify the stream index, it still seeks all streams
+                timestamp: targetPTS,
+                any: false,
+                backward: true,
+            });
+            this.decoder.flush();
+            this.decoder = null;
+            this.decoder = beamcoder.decoder({
+                demuxer: this.demuxer,
+                width: this.demuxer.streams[0].codecpar.width,
+                height: this.demuxer.streams[0].codecpar.height,
+                stream_index: 0, // we initialize the decoder with the first video stream but we still need to specify it further down the line?
+                pix_fmt: this.demuxer.streams[0].codecpar.format,
+                thread_count: 8,
+            });
+            this.packet = null;
+            this.frames = [];
+        }
+        else {
+            // we're looking forward
+        }
 
         // the decoder has been previously flushed while retrieving frames at the end of the stream and has thus been
         // destroyed. See if the requested targetPTS is part of the last few frames we decoded. If so, return it.
         if (!this.decoder) {
-            // console.log('no decoder');
+            console.log('no decoder');
             if ((this.filteredFrames as any).length > 0) {
                 const closestFrame = (this.filteredFrames as any).find(f => f.pts <= targetPTS);
                 // we should probably check the delta between the targetPTS and the closestFrame. If it's too big, we
                 // should return null or something.
                 console.log('returning closest frame with pts', closestFrame.pts);
                 console.log('read', packetReadCount, 'packets');
+                this.previousTargetPTS = targetPTS;
                 return closestFrame;
             }
             throw Error('Unexpected condition: no decoder and no frames');
@@ -224,7 +261,7 @@ export class SimpleExtractor extends BaseExtractor implements Extractor {
 
         // Read packets until we have a frame which is closest to targetPTS
         let filteredFrames = null;
-        let closestFramePTS = 0;
+        let closestFramePTS = -1;
         let outputFrame = null;
 
         // This is the first time we're decoding frames. Get the first packet and decode it.
@@ -235,31 +272,38 @@ export class SimpleExtractor extends BaseExtractor implements Extractor {
         }
         // console.log('packet', !!this.packet, 'frames', this.frames.length, 'pts', this.packet.pts);
 
-        while ((this.packet || this.frames.length !== 0) && closestFramePTS < targetPTS && packetReadCount < 20) {
-            // console.log('packet si:', this.packet?.stream_index, 'pts:', this.packet?.pts, 'frames:', this.frames?.length);
-            // console.log('frames', this.frames?.length, 'frames.pts:', this.frames?.map(f => f.pts), '-> target.pts:', targetPTS);
+        while ((this.packet || this.frames.length !== 0) && closestFramePTS < targetPTS && packetReadCount < MAX_PACKET_READS) {
+            console.log('packet si:', this.packet?.stream_index, 'pts:', this.packet?.pts, 'frames:', this.frames?.length);
+            console.log('frames', this.frames?.length, 'frames.pts:', this.frames?.map(f => f.pts), '-> target.pts:', targetPTS);
 
             // packet contains frames
             if (this.frames.length !== 0) {
                 // filter the frames
                 const filteredResult = await this.filterer.filter([{ name: 'in0:v', frames: this.frames }]);
                 filteredFrames = filteredResult.flatMap(r => r.frames);
-                // console.log('filteredFrames', filteredFrames.length, 'filteredFrames.pts:', filteredFrames.map(f => f.pts), '-> target.pts:', targetPTS);
+                console.log('filteredFrames', filteredFrames.length, 'filteredFrames.pts:', filteredFrames.map(f => f.pts), '-> target.pts:', targetPTS);
                 this.filteredFrames = filteredFrames as beamcoder.DecodedFrames;
 
                 // get the closest frame
-                const closestFrame = filteredFrames.reverse().find(f => f.pts <= targetPTS);
+                let closestFrame;
+                if (filteredFrames.length === 1) {
+                    closestFrame = filteredFrames[0];
+                }
+                else {
+                    closestFrame = filteredFrames.reverse().find(f => f.pts <= targetPTS);
+                }
+
                 closestFramePTS = closestFrame?.pts;
-                // console.log('closestFramePTS', closestFramePTS, 'targetPTS', targetPTS);
+                console.log('closestFramePTS', closestFramePTS, 'targetPTS', targetPTS);
                 if (!outputFrame || closestFramePTS < targetPTS) {
-                    // console.log('|--> assigning outputFrame', closestFrame?.pts);
+                    console.log('|--> assigning outputFrame', closestFrame?.pts);
                     outputFrame = closestFrame;
                 }
                 else {
                     // break out of the loop if we've found the closest frame (and ensure we don't move to the next
                     // packet by calling _getNextPacketAndDecodeFrames again) as this risks us getting a frame that is
                     // after our targetPTS
-                    // console.log('breaking');
+                    console.log('breaking');
                     break;
                 }
             }
@@ -276,6 +320,7 @@ export class SimpleExtractor extends BaseExtractor implements Extractor {
         }
         console.log('read', packetReadCount, 'packets');
 
+        this.previousTargetPTS = targetPTS;
         return outputFrame;
     }
 
@@ -286,12 +331,14 @@ export class SimpleExtractor extends BaseExtractor implements Extractor {
      */
     async _getNextPacketAndDecodeFrames() {
         const packet = await this._getNextVideoStreamPacket();
+        console.log('packet pts:', packet?.pts);
 
         // NOTE: maybe we should only decode frames when we're relatively close to our targetPTS?
         // extract frames from the packet
         let decodedFrames = null;
         if (packet !== null && this.decoder) {
             decodedFrames = await this.decoder.decode(packet as Packet);
+            console.log('decodedFrames', decodedFrames.frames.length, decodedFrames.frames.map(f => f.pts));
         }
         // we've reached the end of the stream
         else {
--- test/simple.test.ts
+++ test/simple.test.ts
@@ -19,17 +19,58 @@ declare global {
 expect.extend({ toMatchImageSnapshot });
 
 describe('simple', () => {
-    it('can get frame', async() => {
+    it('can get frames at random times', async() => {
+        const extractor = await SimpleExtractor.create({
+            inputFileOrUrl: 'https://storage.googleapis.com/lumen5-prod-images/countTo60.mp4',
+        });
+        const times_to_get = [
+            1, // forward to 30
+            0, // backwards to 15
+            1.5, // forward to 45
+            0.5, // backward to 15
+        ];
+        for (let i = 0; i < times_to_get.length; i++) {
+            const imageData = await extractor.getImageDataAtTime(times_to_get[i]);
+            if (!imageData) {
+                continue;
+            }
+            const canvas = createCanvas(imageData.width, imageData.height);
+            const ctx = canvas.getContext('2d');
+            ctx.putImageData(imageData, 0, 0);
+            expect(canvas.toBuffer('image/png')).toMatchImageSnapshot();
+        }
+    });
+
+    it('can get the first 10 frames', async() => {
         const extractor = await SimpleExtractor.create({
             inputFileOrUrl: 'https://storage.googleapis.com/lumen5-prod-images/countTo60.mp4',
         });
         const FRAME_SYNC_DELTA = (1 / 30.0) / 2.0;
 
         // Act & assert
-        // this tests a few tricky situations:
-        // 1. ensures we render the 2nd frame properly - if we read the next packet we'll draw 3 instead of 2
-        // 2. ensures we render the last few frames properly - we have to flush the decoder to get the last few frames
-        for (let i = 0; i < 60; i++) {
+        // ensure we render the 2nd frame properly - if we read the next packet we'll draw 3 instead of 2
+        for (let i = 0; i < 10; i++) {
+            const time = i / 30.0 + FRAME_SYNC_DELTA;
+            const imageData = await extractor.getImageDataAtTime(time);
+            if (!imageData) {
+                continue;
+            }
+            const canvas = createCanvas(imageData.width, imageData.height);
+            const ctx = canvas.getContext('2d');
+            ctx.putImageData(imageData, 0, 0);
+            expect(canvas.toBuffer('image/png')).toMatchImageSnapshot();
+        }
+    });
+
+    it('can get the last 10 frames', async() => {
+        const extractor = await SimpleExtractor.create({
+            inputFileOrUrl: 'https://storage.googleapis.com/lumen5-prod-images/countTo60.mp4',
+        });
+        const FRAME_SYNC_DELTA = (1 / 30.0) / 2.0;
+
+        // Act & assert
+        // ensure we render the last few frames properly - we have to flush the decoder to get the last few frames
+        for (let i = 50; i < 60; i++) {
             const time = i / 30.0 + FRAME_SYNC_DELTA;
             const imageData = await extractor.getImageDataAtTime(time);
             if (!imageData) {
