a92ceac854ab11cee514ed0cd88f6f38ea266ad6
Tommy Hu <tommy@lumen5.com>
2025-11-10 11:32:38 -0800

    Try using latest ffmpeg version.

--- scripts/install_beamcoder_dependencies.sh
+++ scripts/install_beamcoder_dependencies.sh
@@ -4,11 +4,11 @@ ARCH=$(uname -m)
 
 if [[ "$ARCH" == "x86_64" ]]; then
     echo "Detected 64 bit intel"
-    BINARIES_URL="https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2025-01-31-12-58/ffmpeg-n5.1.6-16-g6e63e49496-linux64-gpl-shared-5.1.tar.xz"
+    BINARIES_URL="https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2025-11-10-13-05/ffmpeg-n8.0-32-g44bfe0da61-linux64-gpl-shared-8.0.tar.xz"
 elif [[ "$ARCH" == "aarch64" ]]; then
     # ARM linux setup
     echo "Detected ARM"
-    BINARIES_URL="https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2025-01-31-12-58/ffmpeg-n5.1.6-16-g6e63e49496-linuxarm64-gpl-shared-5.1.tar.xz"
+    BINARIES_URL="https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2025-11-10-13-05/ffmpeg-n8.0-32-g44bfe0da61-linuxarm64-gpl-shared-8.0.tar.xz"
 else
     echo "\n\n\UNSUPPORTED ARCH $ARCH\n\n\n"
     exit 1
